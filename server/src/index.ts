51373712d988d3a52a1c5417346c4d1e06bb3a59
nafisreza <bigganinafis@gmail.com>
2025-10-21 23:32:52 +0600

    chore: hash with bcrypt & /login route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { createPrismaClient } from "./db";
-import { decode, sign, verify } from 'hono/jwt';
+import { decode, sign, verify } from "hono/jwt";
+import bcrypt from "bcryptjs";
 
 type Bindings = {
   DATABASE_URL: string;
@@ -13,12 +14,15 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", async (c) => {
+app.post("/api/v1/user/register", async (c) => {
   try {
     const { name, username, password } = await c.req.json();
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
     const prisma = createPrismaClient(c.env.DATABASE_URL);
     const user = await prisma.user.create({
-      data: { name, username, password },
+      data: { name, username, password: hashedPass },
     });
 
     const jwt = await sign({ userId: user.id }, c.env.JWT_SECRET);
@@ -27,6 +31,29 @@ app.post("/api/v1/user/signup", async (c) => {
   } catch (error) {
     console.error("Error creating user:", error);
     return c.json({ error: "User creation failed" }, 500);
+  }
+});
+
+app.post("/api/v1/user/login", async (c) => {
+  try {
+    const { username, password } = await c.req.json();
+    const prisma = createPrismaClient(c.env.DATABASE_URL);
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) {
+      return c.json({ error: "User does not exist" }, 404);
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return c.json({ error: "Invalid username or password" }, 403);
+    }
+
+    const jwt = await sign({ userId: user.id }, c.env.JWT_SECRET);
+    return c.json({ user, jwt });
+  } catch (error) {
+    console.error("Error during login:", error);
+    return c.json({ error: "Login failed" }, 500);
   }
 });
 
